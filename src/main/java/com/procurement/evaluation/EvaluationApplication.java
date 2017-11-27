7e024cf348b1ed560741a0d745af7720504fd796
Oleh Herashchenko <joghua@gmail.com>
2017-11-27 15:24:53 +0200

    first launch ok

--- src/main/java/com/procurement/evaluation/EvaluationApplication.java
+++ src/main/java/com/procurement/evaluation/EvaluationApplication.java
@@ -1,9 +1,12 @@
 package com.procurement.evaluation;
 
+import com.procurement.evaluation.config.ApplicationConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(
+    scanBasePackageClasses = ApplicationConfig.class
+)
 public class EvaluationApplication {
 
     public static void main(String[] args) {
