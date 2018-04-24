b87443f7777052fbf14c95c487b6ac2beeb5935b
andreykolchev <andreykolchev@gmail.com>
2018-04-24 18:00:33 +0300

    add banner

--- src/main/java/com/procurement/evaluation/model/dto/ocds/OrganizationReference.java
+++ src/main/java/com/procurement/evaluation/model/dto/ocds/OrganizationReference.java
@@ -1,41 +1,80 @@
 package com.procurement.evaluation.model.dto.ocds;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import java.util.HashSet;
+import java.util.Set;
+import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import lombok.Getter;
+import lombok.Setter;
 import org.apache.commons.lang.builder.EqualsBuilder;
 import org.apache.commons.lang.builder.HashCodeBuilder;
 
 @Getter
+@Setter
+@JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonPropertyOrder({
         "id",
-        "name"
+        "name",
+        "identifier",
+        "address",
+        "additionalIdentifiers",
+        "contactPoint"
 })
 public class OrganizationReference {
 
-    @NotNull
     @JsonProperty("id")
-    private final String id;
-
     @NotNull
+    private String id;
+
     @Size(min = 1)
+    @NotNull
     @JsonProperty("name")
     private final String name;
 
+    @Valid
+    @JsonProperty("identifier")
+    private final Identifier identifier;
+
+    @Valid
+    @JsonProperty("additionalIdentifiers")
+    private final Set<Identifier> additionalIdentifiers;
+
+    @Valid
+    @JsonProperty("address")
+    private final Address address;
+
+    @Valid
+    @JsonProperty("contactPoint")
+    private final ContactPoint contactPoint;
+
     @JsonCreator
-    public OrganizationReference(@JsonProperty("name") final String name,
-                                 @JsonProperty("id") final String id) {
+    public OrganizationReference(@JsonProperty("id") final String id,
+                                 @JsonProperty("name") final String name,
+                                 @JsonProperty("identifier") final Identifier identifier,
+                                 @JsonProperty("address") final Address address,
+                                 @JsonProperty("additionalIdentifiers") final HashSet<Identifier> additionalIdentifiers,
+                                 @JsonProperty("contactPoint") final ContactPoint contactPoint) {
         this.id = id;
         this.name = name;
+        this.identifier = identifier;
+        this.address = address;
+        this.additionalIdentifiers = additionalIdentifiers;
+        this.contactPoint = contactPoint;
     }
 
     @Override
     public int hashCode() {
-        return new HashCodeBuilder().append(name)
-                .append(id)
+        return new HashCodeBuilder().append(id)
+                .append(name)
+                .append(identifier)
+                .append(address)
+                .append(additionalIdentifiers)
+                .append(contactPoint)
                 .toHashCode();
     }
 
@@ -48,8 +87,12 @@ public class OrganizationReference {
             return false;
         }
         final OrganizationReference rhs = (OrganizationReference) other;
-        return new EqualsBuilder().append(name, rhs.name)
-                .append(id, rhs.id)
+        return new EqualsBuilder().append(id, rhs.id)
+                .append(name, rhs.name)
+                .append(identifier, rhs.identifier)
+                .append(address, rhs.address)
+                .append(additionalIdentifiers, rhs.additionalIdentifiers)
+                .append(contactPoint, rhs.contactPoint)
                 .isEquals();
     }
 }
