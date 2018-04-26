71334078d51a5d4396414e47c7e197624fd920d5
andreykolchev <andreykolchev@gmail.com>
2018-04-26 15:33:53 +0300

    create awards

--- src/main/java/com/procurement/evaluation/model/dto/selections/SelectionsRequestDto.java
+++ src/main/java/com/procurement/evaluation/model/dto/selections/SelectionsRequestDto.java
@@ -4,20 +4,15 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.procurement.evaluation.model.dto.ocds.Bid;
 import com.procurement.evaluation.model.dto.ocds.Lot;
-
 import java.util.List;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
-
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class SelectionsRequestDto {
-
-    @JsonProperty(value = "awardCriteria")
-    private String awardCriteria;
 
     @Valid
     @NotNull
@@ -30,10 +25,8 @@ public class SelectionsRequestDto {
     private final List<Bid> bids;
 
     @JsonCreator
-    public SelectionsRequestDto(@JsonProperty("awardCriteria") final String awardCriteria,
-                                @JsonProperty("lots") final List<Lot> lots,
+    public SelectionsRequestDto(@JsonProperty("lots") final List<Lot> lots,
                                 @JsonProperty("bids") final List<Bid> bids) {
-        this.awardCriteria = awardCriteria;
         this.lots = lots;
         this.bids = bids;
     }
