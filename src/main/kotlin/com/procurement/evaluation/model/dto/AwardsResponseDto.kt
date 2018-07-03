ee42778b316cc51b715640932dba064f0fa9dcbf
andreykolchev <andreykolchev@gmail.com>
2018-07-03 17:36:19 +0300

    dto

--- src/main/kotlin/com/procurement/evaluation/model/dto/AwardsResponseDto.kt
+++ src/main/kotlin/com/procurement/evaluation/model/dto/AwardsResponseDto.kt
@@ -1,7 +1,6 @@
 package com.procurement.evaluation.model.dto
 
 import com.fasterxml.jackson.annotation.JsonInclude
-import com.fasterxml.jackson.annotation.JsonProperty
 import com.procurement.evaluation.model.dto.ocds.Award
 import com.procurement.evaluation.model.dto.ocds.Lot
 import com.procurement.evaluation.model.dto.ocds.Period
