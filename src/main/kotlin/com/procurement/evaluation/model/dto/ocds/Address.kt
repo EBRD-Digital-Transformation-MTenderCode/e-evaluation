b5283f930e1ba2355fd0140db260b6bf24ed6843
andreykolchev <andreykolchev@gmail.com>
2018-07-31 17:57:59 +0300

    FAIL_ON_UNKNOWN_PROPERTIES

--- src/main/kotlin/com/procurement/evaluation/model/dto/ocds/Address.kt
+++ src/main/kotlin/com/procurement/evaluation/model/dto/ocds/Address.kt
@@ -2,7 +2,9 @@ package com.procurement.evaluation.model.dto.ocds
 
 import com.fasterxml.jackson.annotation.JsonCreator
 import com.fasterxml.jackson.annotation.JsonInclude
+import javax.validation.Valid
 import javax.validation.constraints.NotNull
+
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 data class Address @JsonCreator constructor(
@@ -10,14 +12,62 @@ data class Address @JsonCreator constructor(
         @field:NotNull
         val streetAddress: String,
 
-        @field:NotNull
-        val locality: String,
-
-        @field:NotNull
-        val region: String,
-
         val postalCode: String?,
 
+        @field:Valid @field:NotNull
+        val addressDetails: AddressDetails
+)
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+data class AddressDetails(
+
+        @field:Valid @field:NotNull
+        val country: CountryDetails,
+
+        @field:Valid @field:NotNull
+        val region: RegionDetails,
+
+        @field:Valid @field:NotNull
+        val locality: LocalityDetails
+)
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+data class CountryDetails(
+
+        var scheme: String?,
+
         @field:NotNull
-        val countryName: String
+        val id: String,
+
+        var description: String?,
+
+        var uri: String?
+)
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+data class RegionDetails(
+
+        var scheme: String?,
+
+        @field:NotNull
+        val id: String,
+
+        var description: String?,
+
+        var uri: String?
+)
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+data class LocalityDetails(
+
+        @field:NotNull
+        var scheme: String,
+
+        @field:NotNull
+        val id: String,
+
+        @field:NotNull
+        var description: String,
+
+        var uri: String?
 )
