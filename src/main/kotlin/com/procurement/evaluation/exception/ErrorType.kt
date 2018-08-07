1345384ebd35de9ce361ad89082a1578b3cc194f
Oleh Herashchenko <joghua@gmail.com>
2018-08-07 17:57:28 +0300

    844

--- src/main/kotlin/com/procurement/evaluation/exception/ErrorType.kt
+++ src/main/kotlin/com/procurement/evaluation/exception/ErrorType.kt
@@ -6,6 +6,7 @@ enum class ErrorType constructor(val code: String, val message: String) {
     INVALID_OWNER("00.02", "Invalid owner."),
     INVALID_STATUS("00.03", "Invalid status of award"),
     INVALID_STATUS_DETAILS("00.04", "Invalid status details of award"),
+    INVALID_TOKEN("00.05", "Invalid token."),
     PERIOD_NOT_FOUND("01.01", "Period not found."),
     BIDS_RULES_NOT_FOUND("02.02", "Bids rules not found.");
 }
