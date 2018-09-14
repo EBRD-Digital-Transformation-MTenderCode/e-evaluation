c8019670331019b008620220352fd737266e3fd9
andreykolchev <andreykolchev@gmail.com>
2018-09-14 13:46:58 +0300

    command

--- src/main/kotlin/com/procurement/evaluation/controller/CommandController.kt
+++ src/main/kotlin/com/procurement/evaluation/controller/CommandController.kt
@@ -1,18 +1,15 @@
 package com.procurement.evaluation.controller
 
-import com.procurement.evaluation.model.dto.bpe.CommandMessage
-import com.procurement.evaluation.model.dto.bpe.CommandType
-import com.procurement.evaluation.model.dto.bpe.ResponseDto
+import com.procurement.evaluation.exception.EnumException
+import com.procurement.evaluation.exception.ErrorException
+import com.procurement.evaluation.model.dto.bpe.*
 import com.procurement.evaluation.service.CreateAwardService
 import com.procurement.evaluation.service.StatusService
 import com.procurement.evaluation.service.UpdateAwardService
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.validation.annotation.Validated
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @Validated
 @RestController
@@ -34,6 +31,17 @@ class CommandController(private val createAwardService: CreateAwardService,
             CommandType.SET_FINAL_STATUSES -> statusService.setFinalStatuses(cm)
             CommandType.PREPARE_CANCELLATION -> statusService.prepareCancellation(cm)
             CommandType.AWARDS_CANCELLATION -> statusService.awardsCancellation(cm)
+        }
+    }
+
+    @ResponseBody
+    @ResponseStatus(HttpStatus.OK)
+    @ExceptionHandler(Exception::class)
+    fun exception(ex: Exception): ResponseDto {
+        return when (ex) {
+            is ErrorException -> getErrorExceptionResponseDto(ex)
+            is EnumException -> getEnumExceptionResponseDto(ex)
+            else -> getExceptionResponseDto(ex)
         }
     }
 }
