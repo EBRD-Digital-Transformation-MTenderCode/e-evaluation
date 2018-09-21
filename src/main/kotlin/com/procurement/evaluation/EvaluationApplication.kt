42c83a30a43bb352aa4a9444d50b8189812359e9
Oleh Herashchenko <joghua@gmail.com>
2018-09-21 16:44:02 +0300

    swarm code refactor

--- src/main/kotlin/com/procurement/evaluation/EvaluationApplication.kt
+++ src/main/kotlin/com/procurement/evaluation/EvaluationApplication.kt
@@ -3,10 +3,8 @@ package com.procurement.evaluation
 import com.procurement.evaluation.config.ApplicationConfig
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 
 @SpringBootApplication(scanBasePackageClasses = [ApplicationConfig::class])
-@EnableEurekaClient
 class EvaluationApplication
 
 fun main(args: Array<String>) {
