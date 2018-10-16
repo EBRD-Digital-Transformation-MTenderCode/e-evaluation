3cada01c21c0e4b9d4ee02d9bac8e8af235f53c8
andreykolchev <andreykolchev@gmail.com>
2018-10-16 21:40:17 +0300

    sb

--- src/main/kotlin/com/procurement/evaluation/exception/ErrorType.kt
+++ src/main/kotlin/com/procurement/evaluation/exception/ErrorType.kt
@@ -11,9 +11,9 @@ enum class ErrorType constructor(val code: String, val message: String) {
     PERIOD_NOT_FOUND("01.01", "Period not found."),
     PERIOD_INVALID("01.02", "Invalid period."),
     BIDS_RULES("02.02", "Bids rules not found."),
-    RELATED_LOTS("02.04", "Related lots in docs not found in award related lots(VR 7.4.4)"),
-    DOC_TYPE("02.05", "Invalid document type Award by bid(VR 7.4.9)"),
-    ALREADY_HAVE_ACTIVE_AWARDS("02.06", "Active status already have(BR-7.4.15(1.a)"),
+    RELATED_LOTS("02.04", "Invalid related lots id."),
+    DOC_TYPE("02.05", "Invalid document type."),
+    ALREADY_HAVE_ACTIVE_AWARDS("02.06", "Awards already have active status."),
     STATUS_DETAILS_SAVED_AWARD("02.07", "Saved award have incorrect status details."),
     CONTEXT("20.01", "Context parameter not found.");
 }
