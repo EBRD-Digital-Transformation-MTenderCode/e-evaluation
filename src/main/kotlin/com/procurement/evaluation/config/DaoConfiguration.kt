35af7f391a0065ceeb621ddf1b06c7ae1e01a281
Maxim Sambulat <maxim.sambulat@gmail.com>
2019-07-09 16:34:42 +0300

    Added a package for scanning where repository will be looking for.

--- src/main/kotlin/com/procurement/evaluation/config/DaoConfiguration.kt
+++ src/main/kotlin/com/procurement/evaluation/config/DaoConfiguration.kt
@@ -10,14 +10,19 @@ import org.springframework.context.annotation.Configuration
 
 @Configuration
 @EnableConfigurationProperties(CassandraProperties::class)
-@ComponentScan(basePackages = ["com.procurement.evaluation.dao"])
+@ComponentScan(
+    basePackages = [
+        "com.procurement.evaluation.dao",
+        "com.procurement.evaluation.infrastructure.repository"
+    ]
+)
 class DaoConfiguration constructor(private val cassandraProperties: CassandraProperties) {
 
     internal val cluster: Cluster
         get() = Cluster.builder()
-                .addContactPoints(*cassandraProperties.getContactPoints())
-                .withAuthProvider(PlainTextAuthProvider(cassandraProperties.username, cassandraProperties.password))
-                .build()
+            .addContactPoints(*cassandraProperties.getContactPoints())
+            .withAuthProvider(PlainTextAuthProvider(cassandraProperties.username, cassandraProperties.password))
+            .build()
 
     @Bean
     fun session(): Session {
