4122b1735fdd86f299c215f361f8ed03f17c9d63
Maxim Sambulat <maxim.sambulat@gmail.com>
2019-07-09 12:44:49 +0300

    Added logging of command and response in the controller.

--- src/main/kotlin/com/procurement/evaluation/controller/CommandController.kt
+++ src/main/kotlin/com/procurement/evaluation/controller/CommandController.kt
@@ -2,21 +2,45 @@ package com.procurement.evaluation.controller
 
 import com.procurement.evaluation.exception.EnumException
 import com.procurement.evaluation.exception.ErrorException
-import com.procurement.evaluation.model.dto.bpe.*
+import com.procurement.evaluation.model.dto.bpe.CommandMessage
+import com.procurement.evaluation.model.dto.bpe.ResponseDto
+import com.procurement.evaluation.model.dto.bpe.getEnumExceptionResponseDto
+import com.procurement.evaluation.model.dto.bpe.getErrorExceptionResponseDto
+import com.procurement.evaluation.model.dto.bpe.getExceptionResponseDto
 import com.procurement.evaluation.service.CommandService
+import com.procurement.evaluation.utils.toJson
+import com.procurement.evaluation.utils.toObject
+import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.validation.annotation.Validated
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseBody
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestController
 
 @Validated
 @RestController
 @RequestMapping("/command")
 class CommandController(private val commandService: CommandService) {
+    companion object {
+        private val log = LoggerFactory.getLogger(CommandController::class.java)
+    }
 
     @PostMapping
-    fun command(@RequestBody commandMessage: CommandMessage): ResponseEntity<ResponseDto> {
-        return ResponseEntity(commandService.execute(commandMessage), HttpStatus.OK)
+    fun command(@RequestBody requestBody: String): ResponseEntity<ResponseDto> {
+        if (log.isDebugEnabled)
+            log.debug("RECEIVED COMMAND: '$requestBody'.")
+        val cm: CommandMessage = toObject(CommandMessage::class.java, requestBody)
+
+        val response = commandService.execute(cm)
+
+        if (log.isDebugEnabled)
+            log.debug("RESPONSE (operation-id: '${cm.context.operationId}'): '${toJson(response)}'.")
+        return ResponseEntity(response, HttpStatus.OK)
     }
 
     @ResponseBody
