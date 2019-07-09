7b7c08d4b17a328ff1da26081b12bbaf3a151da5
Maxim Sambulat <maxim.sambulat@gmail.com>
2019-07-09 14:40:17 +0300

    Added a package for scanning where services will be looking for.

--- src/main/kotlin/com/procurement/evaluation/config/ServiceConfig.kt
+++ src/main/kotlin/com/procurement/evaluation/config/ServiceConfig.kt
@@ -4,5 +4,10 @@ import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-@ComponentScan(basePackages = ["com.procurement.evaluation.service"])
+@ComponentScan(
+    basePackages = [
+        "com.procurement.evaluation.service",
+        "com.procurement.evaluation.application.service"
+    ]
+)
 class ServiceConfig
