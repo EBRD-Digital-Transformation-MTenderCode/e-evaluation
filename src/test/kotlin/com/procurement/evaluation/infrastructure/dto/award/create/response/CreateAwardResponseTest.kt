8a6ffbd5bfdf3834f4859951321761a86b7dffee
Maxim Sambulat <maxim.sambulat@gmail.com>
2019-07-09 12:39:08 +0300

    Added 'DTO' of the response for creating an award.

---
+++ src/test/kotlin/com/procurement/evaluation/infrastructure/dto/award/create/response/CreateAwardResponseTest.kt
@@ -0,0 +1,22 @@
+package com.procurement.evaluation.infrastructure.dto.award.create.response
+
+import com.procurement.evaluation.infrastructure.AbstractDTOTestBase
+import org.junit.jupiter.api.Test
+
+class CreateAwardResponseTest : AbstractDTOTestBase<CreateAwardResponse>(CreateAwardResponse::class.java) {
+
+    @Test
+    fun fully() {
+        testBindingAndMapping("json/infrastructure/dto/award/create/response/response_create_award_full.json")
+    }
+
+    @Test
+    fun required1() {
+        testBindingAndMapping("json/infrastructure/dto/award/create/response/response_create_award_required_1.json")
+    }
+
+    @Test
+    fun required2() {
+        testBindingAndMapping("json/infrastructure/dto/award/create/response/response_create_award_required_2.json")
+    }
+}
