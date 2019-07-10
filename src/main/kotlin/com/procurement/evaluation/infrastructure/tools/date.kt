1821363bec1360cb4a0334191c237ab835fe1de4
Maxim Sambulat <maxim.sambulat@gmail.com>
2019-07-10 13:59:50 +0300

    Added extension function for converting Date to LocalDateTime.

--- src/main/kotlin/com/procurement/evaluation/infrastructure/tools/date.kt
+++ src/main/kotlin/com/procurement/evaluation/infrastructure/tools/date.kt
@@ -2,7 +2,11 @@ package com.procurement.evaluation.infrastructure.tools
 
 import com.procurement.evaluation.infrastructure.bind.date.JsonDateTimeFormatter
 import java.time.LocalDateTime
+import java.time.ZoneOffset
+import java.util.*
 
 fun String.toLocalDateTime(): LocalDateTime {
     return LocalDateTime.parse(this, JsonDateTimeFormatter.formatter)
 }
+
+fun Date.toLocalDateTime(): LocalDateTime = this.toInstant().atZone(ZoneOffset.UTC).toLocalDateTime()
