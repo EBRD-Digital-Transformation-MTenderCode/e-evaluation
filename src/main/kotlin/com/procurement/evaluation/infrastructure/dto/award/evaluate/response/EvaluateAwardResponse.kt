3939637a0f4d31bdbd984484a9e2c1deb17e062a
Maxim Sambulat <maxim.sambulat@gmail.com>
2019-07-31 12:40:27 +0300

    ES-1701 Added DTO of a response for EvaluateAward command.

---
+++ src/main/kotlin/com/procurement/evaluation/infrastructure/dto/award/evaluate/response/EvaluateAwardResponse.kt
@@ -0,0 +1,67 @@
+package com.procurement.evaluation.infrastructure.dto.award.evaluate.response
+
+import com.fasterxml.jackson.annotation.JsonInclude
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize
+import com.fasterxml.jackson.databind.annotation.JsonSerialize
+import com.procurement.evaluation.infrastructure.bind.amount.AmountDeserializer
+import com.procurement.evaluation.infrastructure.bind.amount.AmountSerializer
+import com.procurement.evaluation.infrastructure.bind.date.JsonDateTimeDeserializer
+import com.procurement.evaluation.infrastructure.bind.date.JsonDateTimeSerializer
+import com.procurement.evaluation.model.dto.ocds.AwardStatus
+import com.procurement.evaluation.model.dto.ocds.AwardStatusDetails
+import com.procurement.evaluation.model.dto.ocds.DocumentType
+import java.math.BigDecimal
+import java.time.LocalDateTime
+import java.util.*
+
+data class EvaluateAwardResponse(
+    @field:JsonProperty("award") @param:JsonProperty("award") val award: Award
+) {
+    data class Award(
+        @field:JsonProperty("id") @param:JsonProperty("id") val id: UUID,
+
+        @JsonDeserialize(using = JsonDateTimeDeserializer::class)
+        @JsonSerialize(using = JsonDateTimeSerializer::class)
+        @field:JsonProperty("date") @param:JsonProperty("date") val date: LocalDateTime,
+
+        @JsonInclude(JsonInclude.Include.NON_NULL)
+        @field:JsonProperty("description") @param:JsonProperty("description") val description: String?,
+
+        @field:JsonProperty("status") @param:JsonProperty("status") val status: AwardStatus,
+        @field:JsonProperty("statusDetails") @param:JsonProperty("statusDetails") val statusDetails: AwardStatusDetails,
+        @field:JsonProperty("relatedLots") @param:JsonProperty("relatedLots") val relatedLots: List<UUID>,
+        @field:JsonProperty("value") @param:JsonProperty("value") val value: Value,
+        @field:JsonProperty("suppliers") @param:JsonProperty("suppliers") val suppliers: List<Supplier>,
+
+        @JsonInclude(JsonInclude.Include.NON_EMPTY)
+        @field:JsonProperty("documents") @param:JsonProperty("documents") val documents: List<Document>?
+    ) {
+
+        data class Value(
+            @JsonDeserialize(using = AmountDeserializer::class)
+            @JsonSerialize(using = AmountSerializer::class)
+            @field:JsonProperty("amount") @param:JsonProperty("amount") val amount: BigDecimal,
+
+            @field:JsonProperty("currency") @param:JsonProperty("currency") val currency: String
+        )
+
+        data class Supplier(
+            @field:JsonProperty("id") @param:JsonProperty("id") val id: String,
+            @field:JsonProperty("name") @param:JsonProperty("name") val name: String
+        )
+
+        data class Document(
+            @field:JsonProperty("documentType") @param:JsonProperty("documentType") val documentType: DocumentType,
+            @field:JsonProperty("id") @param:JsonProperty("id") val id: UUID,
+
+            @JsonInclude(JsonInclude.Include.NON_NULL)
+            @field:JsonProperty("title") @param:JsonProperty("title") val title: String?,
+
+            @JsonInclude(JsonInclude.Include.NON_NULL)
+            @field:JsonProperty("description") @param:JsonProperty("description") val description: String?,
+
+            @field:JsonProperty("relatedLots") @param:JsonProperty("relatedLots") val relatedLots: List<UUID>
+        )
+    }
+}
