6a753070f251d6bcbc39837897ffc58ce6193db8
Maxim Sambulat <maxim.sambulat@gmail.com>
2019-08-08 14:23:50 +0300

    ES-1756 Added test for 'WinningAwardResponse'.

---
+++ src/test/kotlin/com/procurement/evaluation/infrastructure/dto/award/win/WinningAwardResponseTest.kt
@@ -0,0 +1,18 @@
+package com.procurement.evaluation.infrastructure.dto.award.win
+
+import com.procurement.evaluation.infrastructure.AbstractDTOTestBase
+import com.procurement.evaluation.infrastructure.dto.award.WinningAwardResponse
+import org.junit.jupiter.api.Test
+
+class WinningAwardResponseTest : AbstractDTOTestBase<WinningAwardResponse>(WinningAwardResponse::class.java) {
+
+    @Test
+    fun fully() {
+        testBindingAndMapping("json/infrastructure/dto/award/win/response/response_winning_award_full.json")
+    }
+
+    @Test
+    fun required1() {
+        testBindingAndMapping("json/infrastructure/dto/award/win/response/response_winning_award_required_1.json")
+    }
+}
