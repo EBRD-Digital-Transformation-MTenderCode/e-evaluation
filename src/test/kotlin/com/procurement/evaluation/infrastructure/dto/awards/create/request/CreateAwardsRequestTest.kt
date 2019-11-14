6f0e7a372b886409a881b7d58847c035eb267b18
katherine <riodael@gmail.com>
2019-11-14 16:11:49 +0200

    ES-2159 Added test for CreateAwardsRequest

---
+++ src/test/kotlin/com/procurement/evaluation/infrastructure/dto/awards/create/request/CreateAwardsRequestTest.kt
@@ -0,0 +1,27 @@
+package com.procurement.evaluation.infrastructure.dto.awards.create.request
+
+import com.procurement.evaluation.infrastructure.AbstractDTOTestBase
+import org.junit.jupiter.api.Test
+
+class CreateAwardsRequestTest : AbstractDTOTestBase<CreateAwardsRequest>(CreateAwardsRequest::class.java) {
+
+    @Test
+    fun fully() {
+        testBindingAndMapping("json/infrastructure/dto/awards/create/request/request_create_awards_full.json")
+    }
+
+    @Test
+    fun required1() {
+        testBindingAndMapping("json/infrastructure/dto/awards/create/request/request_create_awards_required_1.json")
+    }
+
+    @Test
+    fun required2() {
+        testBindingAndMapping("json/infrastructure/dto/awards/create/request/request_create_awards_required_2.json")
+    }
+
+    @Test
+    fun required3() {
+        testBindingAndMapping("json/infrastructure/dto/awards/create/request/request_create_awards_required_3.json")
+    }
+}
