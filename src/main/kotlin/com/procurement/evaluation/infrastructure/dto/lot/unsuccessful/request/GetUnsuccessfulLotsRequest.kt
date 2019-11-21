5c9ec92bd02f40cb071c9cde1049cc1899284089
Maxim Sambulat <maxim.sambulat@gmail.com>
2019-11-21 16:16:04 +0200

    ES-2159 Added request DTO for 'getUnsuccessfulLots' command.

---
+++ src/main/kotlin/com/procurement/evaluation/infrastructure/dto/lot/unsuccessful/request/GetUnsuccessfulLotsRequest.kt
@@ -0,0 +1,20 @@
+package com.procurement.evaluation.infrastructure.dto.lot.unsuccessful.request
+
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.procurement.evaluation.domain.model.bid.BidId
+import com.procurement.evaluation.domain.model.lot.LotId
+
+data class GetUnsuccessfulLotsRequest(
+    @field:JsonProperty("bids") @param:JsonProperty("bids") val bids: List<Bid>,
+    @field:JsonProperty("lots") @param:JsonProperty("lots") val lots: List<Lot>
+) {
+
+    data class Lot(
+        @field:JsonProperty("id") @param:JsonProperty("id") val id: LotId
+    )
+
+    data class Bid(
+        @field:JsonProperty("id") @param:JsonProperty("id") val id: BidId,
+        @field:JsonProperty("relatedLots") @param:JsonProperty("relatedLots") val relatedLots: List<LotId>
+    )
+}
---
+++ src/test/kotlin/com/procurement/evaluation/infrastructure/dto/lot/unsuccessful/request/GetUnsuccessfulLotsRequestTest.kt
@@ -0,0 +1,12 @@
+package com.procurement.evaluation.infrastructure.dto.lot.unsuccessful.request
+
+import com.procurement.evaluation.infrastructure.AbstractDTOTestBase
+import org.junit.jupiter.api.Test
+
+class GetUnsuccessfulLotsRequestTest : AbstractDTOTestBase<GetUnsuccessfulLotsRequest>(GetUnsuccessfulLotsRequest::class.java) {
+
+    @Test
+    fun fully() {
+        testBindingAndMapping("json/infrastructure/dto/lot/unsuccessful/request/request_get_unsuccessful_lots_full.json")
+    }
+}
