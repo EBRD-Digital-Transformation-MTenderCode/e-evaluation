f3403e06e9341d45de7a41f1b5646b1fab5c113b
Maxim Sambulat <maxim.sambulat@gmail.com>
2019-11-22 17:42:24 +0200

    ES-2160 Optimized the imports and reformat the code of the 'Award' class.

--- src/main/kotlin/com/procurement/evaluation/model/dto/ocds/Award.kt
+++ src/main/kotlin/com/procurement/evaluation/model/dto/ocds/Award.kt
@@ -2,50 +2,49 @@ package com.procurement.evaluation.model.dto.ocds
 
 import com.fasterxml.jackson.annotation.JsonCreator
 import com.fasterxml.jackson.annotation.JsonInclude
-import java.math.BigDecimal
 import java.time.LocalDateTime
 
 data class Award @JsonCreator constructor(
 
-        @JsonInclude(JsonInclude.Include.NON_NULL)
-        var token: String?,
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    var token: String?,
 
-        val id: String,
+    val id: String,
 
-        @JsonInclude(JsonInclude.Include.NON_NULL)
-        var date: LocalDateTime?,
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    var date: LocalDateTime?,
 
-        @JsonInclude(JsonInclude.Include.NON_NULL)
-        var description: String?,
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    var description: String?,
 
-        @JsonInclude(JsonInclude.Include.NON_NULL)
-        var title: String?,
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    var title: String?,
 
-        var status: AwardStatus,
+    var status: AwardStatus,
 
-        var statusDetails: AwardStatusDetails,
+    var statusDetails: AwardStatusDetails,
 
-        @JsonInclude(JsonInclude.Include.NON_NULL)
-        val value: Value?,
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    val value: Value?,
 
-        @JsonInclude(JsonInclude.Include.NON_NULL)
-        val weightedValue: Value?,
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    val weightedValue: Value?,
 
-        @JsonInclude(JsonInclude.Include.NON_EMPTY)
-        val relatedLots: List<String>,
+    @JsonInclude(JsonInclude.Include.NON_EMPTY)
+    val relatedLots: List<String>,
 
-        @JsonInclude(JsonInclude.Include.NON_NULL)
-        val relatedBid: String?,
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    val relatedBid: String?,
 
-        @JsonInclude(JsonInclude.Include.NON_NULL)
-        val bidDate: LocalDateTime?,
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    val bidDate: LocalDateTime?,
 
-        @JsonInclude(JsonInclude.Include.NON_EMPTY)
-        val suppliers: List<OrganizationReference>?,
+    @JsonInclude(JsonInclude.Include.NON_EMPTY)
+    val suppliers: List<OrganizationReference>?,
 
-        @JsonInclude(JsonInclude.Include.NON_EMPTY)
-        var documents: List<Document>?,
+    @JsonInclude(JsonInclude.Include.NON_EMPTY)
+    var documents: List<Document>?,
 
-        @JsonInclude(JsonInclude.Include.NON_EMPTY)
-        var items: List<Item>?
+    @JsonInclude(JsonInclude.Include.NON_EMPTY)
+    var items: List<Item>?
 )
