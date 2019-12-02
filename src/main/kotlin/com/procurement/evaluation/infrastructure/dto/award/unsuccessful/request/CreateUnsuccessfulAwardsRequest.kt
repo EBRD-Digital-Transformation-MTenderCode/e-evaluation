15e8153f6e07e6d16069108b0b672e5dc77015cf
Maxim Sambulat <maxim.sambulat@gmail.com>
2019-12-02 12:12:46 +0200

    ES-2204 Changed type of attributes from the abstract types to concrete types (example: UUID -> LotId).

--- src/main/kotlin/com/procurement/evaluation/infrastructure/dto/award/unsuccessful/request/CreateUnsuccessfulAwardsRequest.kt
+++ src/main/kotlin/com/procurement/evaluation/infrastructure/dto/award/unsuccessful/request/CreateUnsuccessfulAwardsRequest.kt
@@ -1,13 +1,12 @@
 package com.procurement.evaluation.infrastructure.dto.award.unsuccessful.request
 
-
 import com.fasterxml.jackson.annotation.JsonProperty
-import java.util.*
+import com.procurement.evaluation.domain.model.lot.LotId
 
 data class CreateUnsuccessfulAwardsRequest(
     @field:JsonProperty("lots") @param:JsonProperty("lots") val lots: List<Lot>
 ) {
     data class Lot(
-        @field:JsonProperty("id") @param:JsonProperty("id") val id: UUID
+        @field:JsonProperty("id") @param:JsonProperty("id") val id: LotId
     )
 }
