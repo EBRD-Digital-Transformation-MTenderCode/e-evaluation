adf71d7875f53c6cfc2dd7416aecb24f86b6a5f7
Evgen Kopach <zkopach@gmail.com>
2019-12-12 17:05:01 +0200

    ES-2298 Created tests for request dto at 'createAwardAuctionEnd' command

---
+++ src/test/kotlin/com/procurement/evaluation/infrastructure/dto/awards/create/auction/end/request/CreateAwardsauctionEndRequestTest.kt
@@ -0,0 +1,30 @@
+package com.procurement.evaluation.infrastructure.dto.awards.create.auction.end.request
+
+import com.procurement.evaluation.infrastructure.AbstractDTOTestBase
+import com.procurement.evaluation.infrastructure.dto.award.create.auction.end.request.CreateAwardsAuctionEndRequest
+import org.junit.jupiter.api.Test
+
+class CreateAwardsauctionEndRequestTest : AbstractDTOTestBase<CreateAwardsAuctionEndRequest>(
+    CreateAwardsAuctionEndRequest::class.java
+) {
+
+    @Test
+    fun fully() {
+        testBindingAndMapping("json/infrastructure/dto/awards/create/auction/end/request/request_create_awards_auction_end_full.json")
+    }
+
+    @Test
+    fun required1() {
+        testBindingAndMapping("json/infrastructure/dto/awards/create/auction/end/request/request_create_awards_auction_end_required_1.json")
+    }
+
+    @Test
+    fun required2() {
+        testBindingAndMapping("json/infrastructure/dto/awards/create/auction/end/request/request_create_awards_auction_end_required_2.json")
+    }
+
+    @Test
+    fun required3() {
+        testBindingAndMapping("json/infrastructure/dto/awards/create/auction/end/request/request_create_awards_auction_end_required_3.json")
+    }
+}
