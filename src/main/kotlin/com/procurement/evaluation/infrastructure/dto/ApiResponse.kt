43c6a35549d6875d1f8d4de9dc0f27e3d41106d5
Evgen Kopach <zkopach@gmail.com>
2019-12-24 19:23:32 +0200

    ES-2434 Added ApiResponse object

---
+++ src/main/kotlin/com/procurement/evaluation/infrastructure/dto/ApiResponse.kt
@@ -0,0 +1,21 @@
+package com.procurement.evaluation.infrastructure.dto
+
+import com.procurement.evaluation.model.dto.bpe.ApiVersion
+import com.procurement.evaluation.model.dto.bpe.ResponseErrorDto
+
+sealed class ApiResponse {
+    abstract val id: String?
+    abstract val version: ApiVersion?
+}
+
+class ApiErrorResponse(
+    override val id: String?,
+    override val version: ApiVersion?,
+    val errors: List<ResponseErrorDto>
+) : ApiResponse()
+
+class ApiSuccessResponse(
+    override val id: String?,
+    override val version: ApiVersion?,
+    val data: Any?
+) : ApiResponse()
