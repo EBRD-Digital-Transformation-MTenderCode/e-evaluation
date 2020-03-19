c733739bcc6079d5de113c6a4a08580f31245b5c
katherine <kate.shaidurova@gmail.com>
2020-03-19 10:27:48 +0200

    ES-2644 Added tryExecute method for Session that returns  Result

---
+++ src/main/kotlin/com/procurement/evaluation/infrastructure/extension/cassandra/Session.kt
@@ -0,0 +1,15 @@
+package com.procurement.evaluation.infrastructure.extension.cassandra
+
+import com.datastax.driver.core.BoundStatement
+import com.datastax.driver.core.ResultSet
+import com.datastax.driver.core.Session
+import com.procurement.evaluation.domain.functional.Result
+import com.procurement.evaluation.domain.functional.Result.Companion.failure
+import com.procurement.evaluation.domain.functional.Result.Companion.success
+import com.procurement.evaluation.infrastructure.fail.Fail
+
+fun BoundStatement.tryExecute(session: Session): Result<ResultSet, Fail.Incident.DatabaseInteractionIncident> = try {
+    success(session.execute(this))
+} catch (expected: Exception) {
+    failure(Fail.Incident.DatabaseInteractionIncident(exception = expected))
+}
