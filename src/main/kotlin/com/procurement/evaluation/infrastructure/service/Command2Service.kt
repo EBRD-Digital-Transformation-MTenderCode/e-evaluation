36ab2d0f8a54d5814ab803e2456e431eb9744346
katherine <kate.shaidurova@gmail.com>
2020-03-20 11:58:51 +0200

    ES-2632 Added branch for getAwardStateByIdsHandler handle method execution

--- src/main/kotlin/com/procurement/evaluation/infrastructure/service/Command2Service.kt
+++ src/main/kotlin/com/procurement/evaluation/infrastructure/service/Command2Service.kt
@@ -2,19 +2,20 @@ package com.procurement.evaluation.infrastructure.service
 
 import com.fasterxml.jackson.databind.JsonNode
 import com.procurement.evaluation.infrastructure.dto.ApiResponse2
+import com.procurement.evaluation.infrastructure.handler.GetAwardStateByIdsHandler
 import com.procurement.evaluation.model.dto.bpe.Command2Type
 import com.procurement.evaluation.model.dto.bpe.tryGetAction
 import org.springframework.stereotype.Service
 
 @Service
-class Command2Service {
+class Command2Service(private val getAwardStateByIdsHandler: GetAwardStateByIdsHandler) {
 
     fun execute(node: JsonNode): ApiResponse2 {
         val action = node.tryGetAction().get
 
         return when (action) {
             Command2Type.GET_AWARD_STATES_BY_IDS -> {
-                TODO()
+                getAwardStateByIdsHandler.handle(node)
             }
         }
     }
