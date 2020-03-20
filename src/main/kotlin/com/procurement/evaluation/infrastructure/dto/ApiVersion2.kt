a8582309964c2f3a3732984a216e6c5a0e627768
katherine <kate.shaidurova@gmail.com>
2020-03-20 13:12:48 +0200

    ES-2632 Added valueOf method for use in de/serializers for ApiVersion2

--- src/main/kotlin/com/procurement/evaluation/infrastructure/dto/ApiVersion2.kt
+++ src/main/kotlin/com/procurement/evaluation/infrastructure/dto/ApiVersion2.kt
@@ -4,6 +4,23 @@ import com.procurement.evaluation.domain.functional.Result
 
 data class ApiVersion2(val major: Int, val minor: Int, val patch: Int) {
     companion object {
+        fun valueOf(version: String): ApiVersion2 {
+            val elements = version.split(".")
+            if (elements.isEmpty() || elements.size != 3)
+                throw IllegalArgumentException("Invalid value of the api version ($version).")
+
+            val major: Int = elements[0].toIntOrNull()
+                ?: throw IllegalArgumentException("Invalid value of the api version ($version).")
+
+            val minor: Int = elements[1].toIntOrNull()
+                ?: throw IllegalArgumentException("Invalid value of the api version ($version).")
+
+            val patch: Int = elements[2].toIntOrNull()
+                ?: throw IllegalArgumentException("Invalid value of the api version ($version).")
+
+            return ApiVersion2(major = major, minor = minor, patch = patch)
+        }
+
         fun tryValueOf(version: String): Result<ApiVersion2, String> {
             val elements = version.split(".")
             if (elements.isEmpty() || elements.size != 3)
