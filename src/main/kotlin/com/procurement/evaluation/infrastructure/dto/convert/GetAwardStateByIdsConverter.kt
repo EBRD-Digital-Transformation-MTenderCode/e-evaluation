3f263b2d60b8c35d9f351cb1df409532552fa1ea
katherine <kate.shaidurova@gmail.com>
2020-03-20 11:55:04 +0200

    ES-2632 Added GetAwardStateByIdsHandler and converter

---
+++ src/main/kotlin/com/procurement/evaluation/infrastructure/dto/convert/GetAwardStateByIdsConverter.kt
@@ -0,0 +1,11 @@
+package com.procurement.evaluation.infrastructure.dto.convert
+
+import com.procurement.evaluation.application.model.award.state.GetAwardStateByIdsParams
+import com.procurement.evaluation.domain.functional.Result
+import com.procurement.evaluation.infrastructure.dto.award.state.GetAwardStateByIdsRequest
+import com.procurement.evaluation.infrastructure.fail.error.DataErrors
+
+fun GetAwardStateByIdsRequest.convert(): Result<GetAwardStateByIdsParams, DataErrors> =
+    GetAwardStateByIdsParams.tryCreate(
+        awardIds, cpid, ocid
+    )
