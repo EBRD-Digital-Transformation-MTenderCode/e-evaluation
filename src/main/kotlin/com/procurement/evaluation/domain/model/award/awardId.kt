d1026e5895ddae7802c29ccfe171650722cffe40
katherine <kate.shaidurova@gmail.com>
2020-03-20 11:49:23 +0200

    ES-2632 Added AwardId parsing

--- src/main/kotlin/com/procurement/evaluation/domain/model/award/awardId.kt
+++ src/main/kotlin/com/procurement/evaluation/domain/model/award/awardId.kt
@@ -1,5 +1,11 @@
 package com.procurement.evaluation.domain.model.award
 
+import com.procurement.evaluation.domain.functional.Result
+import com.procurement.evaluation.domain.util.extension.tryUUID
+import com.procurement.evaluation.infrastructure.fail.Fail
 import java.util.*
 
 typealias AwardId = UUID
+
+fun String.tryAwardId(): Result<AwardId, Fail.Incident.Parsing> =
+    this.tryUUID()
