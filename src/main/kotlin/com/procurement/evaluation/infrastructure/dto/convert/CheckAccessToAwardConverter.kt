7016d440de0b8faedba2f34ef1feed0cd98f710e
katherine <kate.shaidurova@gmail.com>
2020-03-24 12:00:10 +0200

    ES-2631 Added CheckAccessToAwardConverter

---
+++ src/main/kotlin/com/procurement/evaluation/infrastructure/dto/convert/CheckAccessToAwardConverter.kt
@@ -0,0 +1,11 @@
+package com.procurement.evaluation.infrastructure.dto.convert
+
+import com.procurement.evaluation.application.model.award.state.CheckAccessToAwardParams
+import com.procurement.evaluation.domain.functional.Result
+import com.procurement.evaluation.infrastructure.dto.award.access.CheckAccessToAwardRequest
+import com.procurement.evaluation.infrastructure.fail.error.DataErrors
+
+fun CheckAccessToAwardRequest.convert(): Result<CheckAccessToAwardParams, DataErrors> =
+    CheckAccessToAwardParams.tryCreate(
+        cpid, ocid, token, owner, awardId
+    )
