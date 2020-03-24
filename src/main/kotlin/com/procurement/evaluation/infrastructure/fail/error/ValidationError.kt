6e1ad51cc0dbfb38c81cd838a0f608a0f2c81f3f
katherine <kate.shaidurova@gmail.com>
2020-03-24 16:45:31 +0200

    ES-2631 Added AwardNotFound to validation errors

--- src/main/kotlin/com/procurement/evaluation/infrastructure/fail/error/ValidationError.kt
+++ src/main/kotlin/com/procurement/evaluation/infrastructure/fail/error/ValidationError.kt
@@ -8,4 +8,6 @@ sealed class ValidationError(numberError: String, override val description: Stri
     class InvalidToken() : ValidationError(numberError = "10.2.4.1", description = "Request token doesn't match token from the database.")
 
     class InvalidOwner() : ValidationError(numberError = "10.2.4.2", description = "Request owner doesn't match owner from the database.")
+
+    class AwardNotFound() : ValidationError(numberError = "10.4.2.3", description = "Award not found.")
 }
