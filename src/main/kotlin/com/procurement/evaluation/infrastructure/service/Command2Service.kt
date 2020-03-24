1cd006c132f92a611c3651f1f7ed17127de9e4e8
katherine <kate.shaidurova@gmail.com>
2020-03-24 12:09:37 +0200

    ES-2631 Added CHECK_ACCESS_TO_AWARD branch to Command2Service

--- src/main/kotlin/com/procurement/evaluation/infrastructure/service/Command2Service.kt
+++ src/main/kotlin/com/procurement/evaluation/infrastructure/service/Command2Service.kt
@@ -2,13 +2,15 @@ package com.procurement.evaluation.infrastructure.service
 
 import com.fasterxml.jackson.databind.JsonNode
 import com.procurement.evaluation.infrastructure.dto.ApiResponse2
+import com.procurement.evaluation.infrastructure.handler.CheckAccessToAwardHandler
 import com.procurement.evaluation.infrastructure.handler.GetAwardStateByIdsHandler
 import com.procurement.evaluation.model.dto.bpe.Command2Type
 import com.procurement.evaluation.model.dto.bpe.tryGetAction
 import org.springframework.stereotype.Service
 
 @Service
-class Command2Service(private val getAwardStateByIdsHandler: GetAwardStateByIdsHandler) {
+class Command2Service(private val getAwardStateByIdsHandler: GetAwardStateByIdsHandler,
+                      private val checkAccessToAwardHandler: CheckAccessToAwardHandler) {
 
     fun execute(node: JsonNode): ApiResponse2 {
         val action = node.tryGetAction().get
@@ -16,6 +18,9 @@ class Command2Service(private val getAwardStateByIdsHandler: GetAwardStateByIdsH
         return when (action) {
             Command2Type.GET_AWARD_STATES_BY_IDS -> {
                 getAwardStateByIdsHandler.handle(node)
+            }
+            Command2Type.CHECK_ACCESS_TO_AWARD -> {
+                checkAccessToAwardHandler.handle(node)
             }
         }
     }
