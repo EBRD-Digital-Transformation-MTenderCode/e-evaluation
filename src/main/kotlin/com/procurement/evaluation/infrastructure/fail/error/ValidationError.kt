c331df9060b82c04d8ee2bd1941df65cc51fcbb4
katherine <kate.shaidurova@gmail.com>
2020-03-24 12:57:38 +0200

    ES-2631 Fixed message for ValidationErrors

--- src/main/kotlin/com/procurement/evaluation/infrastructure/fail/error/ValidationError.kt
+++ src/main/kotlin/com/procurement/evaluation/infrastructure/fail/error/ValidationError.kt
@@ -5,7 +5,7 @@ import com.procurement.evaluation.infrastructure.fail.Fail
 sealed class ValidationError(numberError: String, override val description: String) : Fail.Error("VR-") {
     override val code: String = prefix + numberError
 
-    class InvalidToken() : ValidationError(numberError = "10.2.4.1", description = "Request token doesn't match token from database.")
+    class InvalidToken() : ValidationError(numberError = "10.2.4.1", description = "Request token doesn't match token from the database.")
 
-    class InvalidOwner() : ValidationError(numberError = "10.2.4.2", description = "Request owner doesn't match owner from database.")
+    class InvalidOwner() : ValidationError(numberError = "10.2.4.2", description = "Request owner doesn't match owner from the database.")
 }
