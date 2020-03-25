c2040a9b74508e2863e134eb745f54730261a893
katherine <kate.shaidurova@gmail.com>
2020-03-25 12:38:57 +0200

    ES-2635 Added 'checkRelatedTenderer' branch to Command2Service

--- src/main/kotlin/com/procurement/evaluation/infrastructure/service/Command2Service.kt
+++ src/main/kotlin/com/procurement/evaluation/infrastructure/service/Command2Service.kt
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.JsonNode
 import com.procurement.evaluation.application.service.Logger
 import com.procurement.evaluation.infrastructure.dto.ApiResponse2
 import com.procurement.evaluation.infrastructure.handler.CheckAccessToAwardHandler
+import com.procurement.evaluation.infrastructure.handler.CheckRelatedTendererHandler
 import com.procurement.evaluation.infrastructure.handler.GetAwardStateByIdsHandler
 import com.procurement.evaluation.model.dto.bpe.Command2Type
 import com.procurement.evaluation.model.dto.bpe.generateResponseOnFailure
@@ -16,7 +17,8 @@ import org.springframework.stereotype.Service
 class Command2Service(
     private val logger: Logger,
     private val getAwardStateByIdsHandler: GetAwardStateByIdsHandler,
-    private val checkAccessToAwardHandler: CheckAccessToAwardHandler
+    private val checkAccessToAwardHandler: CheckAccessToAwardHandler,
+    private val checkRelatedTendererHandler: CheckRelatedTendererHandler
 ) {
 
     fun execute(node: JsonNode): ApiResponse2 {
@@ -31,12 +33,14 @@ class Command2Service(
             }
 
         return when (action) {
-            Command2Type.GET_AWARD_STATES_BY_IDS -> {
+            Command2Type.GET_AWARD_STATES_BY_IDS ->
                 getAwardStateByIdsHandler.handle(node)
-            }
-            Command2Type.CHECK_ACCESS_TO_AWARD -> {
+
+            Command2Type.CHECK_ACCESS_TO_AWARD ->
                 checkAccessToAwardHandler.handle(node)
-            }
+
+            Command2Type.CHECK_RELATED_TENDERER ->
+                checkRelatedTendererHandler.handle(node)
         }
     }
 }
