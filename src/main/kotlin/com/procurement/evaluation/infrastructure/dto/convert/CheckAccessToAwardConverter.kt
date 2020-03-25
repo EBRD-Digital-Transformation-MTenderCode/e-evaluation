557dbd6c58ef0f9ee49b1fefcda8aa3c59c6cc9e
katherine <kate.shaidurova@gmail.com>
2020-03-25 11:45:55 +0200

    ES-2635 Fixed imports after CheckAccessToAwardParams had been moved

--- src/main/kotlin/com/procurement/evaluation/infrastructure/dto/convert/CheckAccessToAwardConverter.kt
+++ src/main/kotlin/com/procurement/evaluation/infrastructure/dto/convert/CheckAccessToAwardConverter.kt
@@ -1,6 +1,6 @@
 package com.procurement.evaluation.infrastructure.dto.convert
 
-import com.procurement.evaluation.application.model.award.state.CheckAccessToAwardParams
+import com.procurement.evaluation.application.model.award.access.CheckAccessToAwardParams
 import com.procurement.evaluation.domain.functional.Result
 import com.procurement.evaluation.infrastructure.dto.award.access.CheckAccessToAwardRequest
 import com.procurement.evaluation.infrastructure.fail.error.DataErrors
