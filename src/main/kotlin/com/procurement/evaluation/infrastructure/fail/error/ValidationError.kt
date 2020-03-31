a4b8bd757eae0dc66ff76860a8c9bd59e69e62f3
katherine <kate.shaidurova@gmail.com>
2020-03-31 19:09:31 +0300

    ES-2780 Added id field to ValidationError

--- src/main/kotlin/com/procurement/evaluation/infrastructure/fail/error/ValidationError.kt
+++ src/main/kotlin/com/procurement/evaluation/infrastructure/fail/error/ValidationError.kt
@@ -1,22 +1,48 @@
 package com.procurement.evaluation.infrastructure.fail.error
 
+import com.procurement.evaluation.domain.model.award.AwardId
 import com.procurement.evaluation.infrastructure.fail.Fail
 
-sealed class ValidationError(numberError: String, override val description: String) : Fail.Error("VR-") {
+sealed class ValidationError(
+    numberError: String, override val description: String, val id: String? = null
+) : Fail.Error("VR-") {
     override val code: String = prefix + numberError
 
-    class InvalidToken() : ValidationError(numberError = "10.2.4.1", description = "Request token doesn't match token from the database.")
+    class InvalidToken() : ValidationError(
+        numberError = "10.2.4.1",
+        description = "Request token doesn't match token from the database."
+    )
 
-    class InvalidOwner() : ValidationError(numberError = "10.2.4.2", description = "Request owner doesn't match owner from the database.")
+    class InvalidOwner() : ValidationError(
+        numberError = "10.2.4.2",
+        description = "Request owner doesn't match owner from the database."
+    )
 
-    class AwardNotFoundOnCheckRelatedTenderer() : ValidationError(numberError = "10.4.5.1", description = "Award not found.")
+    class AwardNotFoundOnCheckRelatedTenderer(id: AwardId) : ValidationError(
+        numberError = "10.4.5.1",
+        description = "Award not found.",
+        id = id.toString()
+    )
 
-    class TendererNotLinkedToAward() : ValidationError(numberError = "10.4.5.2", description = "Tenderer is not linked to award.")
+    class TendererNotLinkedToAward() : ValidationError(
+        numberError = "10.4.5.2",
+        description = "Tenderer is not linked to award."
+    )
 
-    class DuplicateRequirementResponse() : ValidationError(numberError = "10.4.5.3", description = "Duplicate requirement response.")
+    class DuplicateRequirementResponse() : ValidationError(
+        numberError = "10.4.5.3",
+        description = "Duplicate requirement response."
+    )
 
-    class AwardNotFoundOnCheckAccess() : ValidationError(numberError = "10.4.2.3", description = "Award not found.")
+    class AwardNotFoundOnCheckAccess(id: AwardId) : ValidationError(
+        numberError = "10.4.2.3",
+        description = "Award not found.",
+        id = id.toString()
+    )
 
-    class AwardNotFoundOnCreateRequirementRs() : ValidationError(numberError = "10.4.4.1", description = "Award not found.")
-
+    class AwardNotFoundOnCreateRequirementRs(id: AwardId) : ValidationError(
+        numberError = "10.4.4.1",
+        description = "Award not found.",
+        id = id.toString()
+    )
 }
