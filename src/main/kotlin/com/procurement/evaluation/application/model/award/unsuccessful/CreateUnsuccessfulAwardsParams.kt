eff7d55ec8b1a25f02d10aeec4a05b6b5328f0f1
Yevhenii Skoryi <yevheniiskoryi@gmail.com>
2020-04-07 10:27:33 +0300

    ES-2817 added application level dto  for command 'createUnsuccessfulAwards'

---
+++ src/main/kotlin/com/procurement/evaluation/application/model/award/unsuccessful/CreateUnsuccessfulAwardsParams.kt
@@ -0,0 +1,83 @@
+package com.procurement.evaluation.application.model.award.unsuccessful
+
+import com.procurement.evaluation.application.model.parseCpid
+import com.procurement.evaluation.application.model.parseDate
+import com.procurement.evaluation.application.model.parseOcid
+import com.procurement.evaluation.application.model.parseOwner
+import com.procurement.evaluation.domain.functional.Result
+import com.procurement.evaluation.domain.functional.asFailure
+import com.procurement.evaluation.domain.functional.asSuccess
+import com.procurement.evaluation.domain.model.Cpid
+import com.procurement.evaluation.domain.model.Ocid
+import com.procurement.evaluation.domain.model.Owner
+import com.procurement.evaluation.domain.model.lot.LotId
+import com.procurement.evaluation.domain.model.lot.tryLotId
+import com.procurement.evaluation.infrastructure.fail.error.DataErrors
+import com.procurement.evaluation.lib.toSetBy
+import java.time.LocalDateTime
+
+data class CreateUnsuccessfulAwardsParams private constructor(
+    val owner:Owner,
+    val cpid: Cpid,
+    val ocid: Ocid,
+    val lotIds: List<LotId>,
+    val requestDate: LocalDateTime
+) {
+    companion object {
+        fun tryCreate(
+            owner: String,
+            cpid: String,
+            ocid: String,
+            lotIds: List<String>,
+            requestDate: String
+        ): Result<CreateUnsuccessfulAwardsParams, DataErrors> {
+
+            if (lotIds.isEmpty())
+                return Result.failure(DataErrors.Validation.EmptyArray(name = "lotIds"))
+
+            val uniqueLotsIds = lotIds
+                .toSetBy { it }
+                .toList()
+
+            val nonUniqueIds = lotIds - uniqueLotsIds
+            if (nonUniqueIds.isNotEmpty())
+                return Result.failure(
+                    DataErrors.Validation.UniquenessDataMismatch(
+                        name = "lotIds",
+                        value = nonUniqueIds.joinToString { it }
+                    )
+                )
+
+            val lotIdsParsed = lotIds.map {
+                it.tryLotId()
+                    .doReturn { error ->
+                        return DataErrors.Validation.DataFormatMismatch(
+                            name = "lotIds",
+                            actualValue = it,
+                            expectedFormat = "uuid"
+                        ).asFailure()
+                    }
+            }
+
+            val parsedOwner = parseOwner(value = owner)
+                .doReturn { error -> return error.asFailure() }
+
+            val parsedCpid = parseCpid(value = cpid)
+                .doReturn { error -> return error.asFailure() }
+
+            val parsedOcid = parseOcid(value = ocid)
+                .doReturn { error -> return error.asFailure() }
+
+            val parsedRequestDate = parseDate(value = requestDate, attributeName = "requestDate")
+                .doReturn { error -> return error.asFailure() }
+
+            return CreateUnsuccessfulAwardsParams(
+                cpid = parsedCpid,
+                ocid = parsedOcid,
+                lotIds = lotIdsParsed,
+                requestDate = parsedRequestDate,
+                owner = parsedOwner
+            ).asSuccess()
+        }
+    }
+}
