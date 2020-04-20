5067948ad2d730a882b377751e9c7be783340a0c
katherine <kate.shaidurova@gmail.com>
2020-04-20 14:06:43 +0300

    ES-2977 AddRequirementResponseHandler became a child class of AbstractValidationHandler instead of AbstractHistoricalHandler

--- src/main/kotlin/com/procurement/evaluation/infrastructure/handler/create/requirementresponsehandler/AddRequirementResponseHandler.kt
+++ src/main/kotlin/com/procurement/evaluation/infrastructure/handler/create/requirementresponsehandler/AddRequirementResponseHandler.kt
@@ -1,39 +1,30 @@
 package com.procurement.evaluation.infrastructure.handler.create.requirementresponsehandler
 
 import com.fasterxml.jackson.databind.JsonNode
-import com.procurement.evaluation.application.model.award.requirement.response.CreateRequirementResponseResult
-import com.procurement.evaluation.application.repository.HistoryRepository
 import com.procurement.evaluation.application.service.Logger
 import com.procurement.evaluation.application.service.award.AwardService
-import com.procurement.evaluation.domain.functional.Result
-import com.procurement.evaluation.infrastructure.dto.ApiSuccessResponse2
+import com.procurement.evaluation.domain.functional.ValidationResult
 import com.procurement.evaluation.infrastructure.dto.award.create.requirement.response.AddRequirementResponseRequest
 import com.procurement.evaluation.infrastructure.dto.convert.convert
 import com.procurement.evaluation.infrastructure.fail.Fail
-import com.procurement.evaluation.infrastructure.handler.AbstractHistoricalHandler
+import com.procurement.evaluation.infrastructure.handler.AbstractValidationHandler
 import com.procurement.evaluation.model.dto.bpe.Command2Type
 import com.procurement.evaluation.model.dto.bpe.tryGetParams
 import org.springframework.stereotype.Component
 
 @Component
 class AddRequirementResponseHandler(
-    private val awardService: AwardService,
-    historyRepository: HistoryRepository,
-    logger: Logger
-) : AbstractHistoricalHandler<Command2Type, CreateRequirementResponseResult>(
-    logger = logger,
-    historyRepository = historyRepository,
-    target = ApiSuccessResponse2::class.java
-) {
+    private val awardService: AwardService, logger: Logger
+) : AbstractValidationHandler<Command2Type, Fail>(logger) {
 
     override val action: Command2Type = Command2Type.ADD_REQUIREMENT_RESPONSE
 
-    override fun execute(node: JsonNode): Result<CreateRequirementResponseResult, Fail> {
+    override fun execute(node: JsonNode): ValidationResult<Fail> {
         val params = node
             .tryGetParams(AddRequirementResponseRequest::class.java)
-            .forwardResult { result -> return result }
+            .doReturn { error -> return ValidationResult.error(error) }
             .convert()
-            .forwardResult { result -> return result }
+            .doReturn { error -> return ValidationResult.error(error) }
 
         return awardService.addRequirementResponse(params = params)
     }
