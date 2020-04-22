97667ab8854399aef474eaabf8f7ea77261ac816
katherine <kate.shaidurova@gmail.com>
2020-04-22 19:12:29 +0300

    ES-2977 Added AddRequirementResponseRequestTest.kt

---
+++ src/test/kotlin/com/procurement/evaluation/infrastructure/dto/award/create/requirement/response/AddRequirementResponseRequestTest.kt
@@ -0,0 +1,14 @@
+package com.procurement.evaluation.infrastructure.dto.award.create.requirement.response
+
+import com.procurement.evaluation.infrastructure.AbstractDTOTestBase
+import org.junit.jupiter.api.Test
+
+internal class AddRequirementResponseRequestTest : AbstractDTOTestBase<AddRequirementResponseRequest>(
+    AddRequirementResponseRequest::class.java
+) {
+
+    @Test
+    fun fully() {
+        testBindingAndMapping("json/infrastructure/dto/award/create/requirement/response/add_requirement_response_full.json")
+    }
+}
