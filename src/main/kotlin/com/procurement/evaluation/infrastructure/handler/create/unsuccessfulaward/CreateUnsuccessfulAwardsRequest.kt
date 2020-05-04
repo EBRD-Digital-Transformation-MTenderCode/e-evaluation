aa4ad08dc51f6d6b6163f1bf646724862085ed54
Maxim Sambulat <maxim.sambulat@gmail.com>
2020-05-04 16:39:17 +0300

    ES-3191 Removed 'owner' attribute in 'CreateUnsuccessfulAwardsRequest'.

--- src/main/kotlin/com/procurement/evaluation/infrastructure/handler/create/unsuccessfulaward/CreateUnsuccessfulAwardsRequest.kt
+++ src/main/kotlin/com/procurement/evaluation/infrastructure/handler/create/unsuccessfulaward/CreateUnsuccessfulAwardsRequest.kt
@@ -6,7 +6,6 @@ import com.fasterxml.jackson.annotation.JsonProperty
 data class CreateUnsuccessfulAwardsRequest(
     @field:JsonProperty("cpid") @param:JsonProperty("cpid") val cpid: String,
     @field:JsonProperty("ocid") @param:JsonProperty("ocid") val ocid: String,
-    @field:JsonProperty("owner") @param:JsonProperty("owner") val owner: String,
     @field:JsonProperty("lotIds") @param:JsonProperty("lotIds") val lotIds: List<String>,
     @field:JsonProperty("date") @param:JsonProperty("date") val date: String
 )
