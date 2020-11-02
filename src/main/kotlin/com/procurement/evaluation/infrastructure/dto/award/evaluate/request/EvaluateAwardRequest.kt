8b6e1fd4c9e86cf5379aa591a4a39c92196b6267
katherine <kate.shaidurova@gmail.com>
2020-11-02 12:32:14 +0200

    ES-4685 Added value to request and data

--- src/main/kotlin/com/procurement/evaluation/infrastructure/dto/award/evaluate/request/EvaluateAwardRequest.kt
+++ src/main/kotlin/com/procurement/evaluation/infrastructure/dto/award/evaluate/request/EvaluateAwardRequest.kt
@@ -2,10 +2,15 @@ package com.procurement.evaluation.infrastructure.dto.award.evaluate.request
 
 import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize
+import com.fasterxml.jackson.databind.annotation.JsonSerialize
 import com.procurement.evaluation.domain.model.document.DocumentId
 import com.procurement.evaluation.domain.model.lot.LotId
+import com.procurement.evaluation.infrastructure.bind.amount.AmountDeserializer
+import com.procurement.evaluation.infrastructure.bind.amount.AmountSerializer
 import com.procurement.evaluation.model.dto.ocds.AwardStatusDetails
 import com.procurement.evaluation.model.dto.ocds.DocumentType
+import java.math.BigDecimal
 
 data class EvaluateAwardRequest(
     @field:JsonProperty("award") @param:JsonProperty("award") val award: Award
@@ -17,9 +22,11 @@ data class EvaluateAwardRequest(
         @field:JsonProperty("description") @param:JsonProperty("description") val description: String?,
 
         @JsonInclude(JsonInclude.Include.NON_EMPTY)
-        @field:JsonProperty("documents") @param:JsonProperty("documents") val documents: List<Document>?
-    ) {
+        @field:JsonProperty("documents") @param:JsonProperty("documents") val documents: List<Document>?,
 
+        @JsonInclude(JsonInclude.Include.NON_EMPTY)
+        @field:JsonProperty("value") @param:JsonProperty("value") val value: Value?
+    ) {
         data class Document(
             @field:JsonProperty("id") @param:JsonProperty("id") val id: DocumentId,
 
@@ -32,6 +39,11 @@ data class EvaluateAwardRequest(
             @JsonInclude(JsonInclude.Include.NON_EMPTY)
             @field:JsonProperty("relatedLots") @param:JsonProperty("relatedLots") val relatedLots: List<LotId>?,
             @field:JsonProperty("documentType") @param:JsonProperty("documentType") val documentType: DocumentType
+        )
+        data class Value(
+            @JsonDeserialize(using = AmountDeserializer::class)
+            @JsonSerialize(using = AmountSerializer::class)
+            @field:JsonProperty("amount") @param:JsonProperty("amount") val amount: BigDecimal
         )
     }
 }
