4eee400894a7414a61d37c9e244a538a1ae83d83
Evgen Kopach <zkopach@gmail.com>
2020-11-10 13:30:33 +0200

    ES-4788 Refactored HistoryDao.kt.

--- src/main/kotlin/com/procurement/evaluation/dao/HistoryDao.kt
+++ src/main/kotlin/com/procurement/evaluation/dao/HistoryDao.kt
@@ -5,6 +5,7 @@ import com.datastax.driver.core.querybuilder.QueryBuilder.eq
 import com.datastax.driver.core.querybuilder.QueryBuilder.insertInto
 import com.datastax.driver.core.querybuilder.QueryBuilder.select
 import com.procurement.evaluation.infrastructure.dto.ApiSuccessResponse
+import com.procurement.evaluation.infrastructure.repository.Database
 import com.procurement.evaluation.model.entity.HistoryEntity
 import com.procurement.evaluation.utils.localNowUTC
 import com.procurement.evaluation.utils.toDate
@@ -16,41 +17,34 @@ class HistoryDao(private val session: Session) {
 
     fun getHistory(operationId: String, command: String): HistoryEntity? {
         val query = select()
-                .all()
-                .from(HISTORY_TABLE)
-                .where(eq(COMMAND_ID, operationId))
-                .and(eq(COMMAND_NAME, command))
-                .limit(1)
+            .all()
+            .from(Database.History.TABLE)
+            .where(eq(Database.History.COMMAND_ID, operationId))
+            .and(eq(Database.History.COMMAND_NAME, command))
+            .limit(1)
         val row = session.execute(query).one()
         return if (row != null) HistoryEntity(
-            row.getString(COMMAND_ID),
-            row.getString(COMMAND_NAME),
-            row.getTimestamp(COMMAND_DATE),
-            row.getString(JSON_DATA)) else null
+            row.getString(Database.History.COMMAND_ID),
+            row.getString(Database.History.COMMAND_NAME),
+            row.getTimestamp(Database.History.COMMAND_DATE),
+            row.getString(Database.History.JSON_DATA)
+        ) else null
     }
 
     fun saveHistory(operationId: String, command: String, response: ApiSuccessResponse): HistoryEntity {
         val entity = HistoryEntity(
-                operationId = operationId,
-                command = command,
-                operationDate = localNowUTC().toDate(),
-                jsonData = toJson(response))
+            operationId = operationId,
+            command = command,
+            operationDate = localNowUTC().toDate(),
+            jsonData = toJson(response)
+        )
 
-        val insert = insertInto(HISTORY_TABLE)
-                .value(COMMAND_ID, entity.operationId)
-                .value(COMMAND_NAME, entity.command)
-                .value(COMMAND_DATE, entity.operationDate)
-                .value(JSON_DATA, entity.jsonData)
+        val insert = insertInto(Database.History.TABLE)
+            .value(Database.History.COMMAND_ID, entity.operationId)
+            .value(Database.History.COMMAND_NAME, entity.command)
+            .value(Database.History.COMMAND_DATE, entity.operationDate)
+            .value(Database.History.JSON_DATA, entity.jsonData)
         session.execute(insert)
         return entity
     }
-
-    companion object {
-        private const val HISTORY_TABLE = "evaluation_history"
-        private const val COMMAND_ID = "command_id"
-        private const val COMMAND_NAME = "command_name"
-        private const val COMMAND_DATE = "command_date"
-        private const val JSON_DATA = "json_data"
-    }
-
 }
