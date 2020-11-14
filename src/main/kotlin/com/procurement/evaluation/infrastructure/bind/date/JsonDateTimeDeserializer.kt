4d184751ecfb8d6e893560847bb258a46eb5aaea
Maxim Sambulat <maxim.sambulat@gmail.com>
2020-11-14 14:52:08 +0200

    ES-4788 Refactored JSON date-time serializer and deserializer.

--- src/main/kotlin/com/procurement/evaluation/infrastructure/bind/date/JsonDateTimeDeserializer.kt
+++ src/main/kotlin/com/procurement/evaluation/infrastructure/bind/date/JsonDateTimeDeserializer.kt
@@ -3,10 +3,15 @@ package com.procurement.evaluation.infrastructure.bind.date
 import com.fasterxml.jackson.core.JsonParser
 import com.fasterxml.jackson.databind.DeserializationContext
 import com.fasterxml.jackson.databind.JsonDeserializer
+import com.procurement.evaluation.domain.util.extension.parseLocalDateTime
 import java.time.LocalDateTime
 
 class JsonDateTimeDeserializer : JsonDeserializer<LocalDateTime>() {
 
+    companion object {
+        fun deserialize(value: String): LocalDateTime = value.parseLocalDateTime()
+    }
+
     override fun deserialize(jsonParser: JsonParser, deserializationContext: DeserializationContext): LocalDateTime =
-        LocalDateTime.parse(jsonParser.text, JsonDateTimeFormatter.formatter)
+        deserialize(jsonParser.text)
 }
