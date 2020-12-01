4d2a2a3eaaf057e565c404057943a4ddb89fa520
Evgen Kopach <zkopach@gmail.com>
2020-12-01 17:18:45 +0200

    ES-4963 Created request DTO and test for it.

---
+++ src/test/kotlin/com/procurement/evaluation/infrastructure/dto/award/create/CreateAwardRequestTest.kt
@@ -0,0 +1,28 @@
+package com.procurement.evaluation.infrastructure.dto.award.create
+
+import com.procurement.evaluation.infrastructure.AbstractDTOTestBase
+import com.procurement.evaluation.infrastructure.handler.v2.model.request.CreateAwardRequest
+import org.junit.jupiter.api.Test
+
+class CreateAwardRequestTest : AbstractDTOTestBase<CreateAwardRequest>(CreateAwardRequest::class.java) {
+
+    @Test
+    fun fully() {
+        testBindingAndMapping("json/infrastructure/dto/award/create/create_award_data_full.json")
+    }
+
+    @Test
+    fun required1() {
+        testBindingAndMapping("json/infrastructure/dto/award/create/create_award_data_required_1.json")
+    }
+
+    @Test
+    fun required2() {
+        testBindingAndMapping("json/infrastructure/dto/award/create/create_award_data_required_2.json")
+    }
+
+    @Test
+    fun required3() {
+        testBindingAndMapping("json/infrastructure/dto/award/create/create_award_data_required_3.json")
+    }
+}
