7925b07223cb263244c1a0cef33a2a7961846725
katherine <kate.shaidurova@gmail.com>
2021-01-20 16:33:23 +0200

    ES-5360 Fixed DataType.kt

--- src/main/kotlin/com/procurement/evaluation/model/dto/ocds/DataType.kt
+++ src/main/kotlin/com/procurement/evaluation/model/dto/ocds/DataType.kt
@@ -1,15 +1,21 @@
 package com.procurement.evaluation.model.dto.ocds
 
+import com.fasterxml.jackson.annotation.JsonCreator
+import com.fasterxml.jackson.annotation.JsonValue
 import com.procurement.evaluation.domain.model.enums.EnumElementProvider
 
-enum class DataType(override val key: String) : EnumElementProvider.Key {
+enum class DataType(@JsonValue override val key: String) : EnumElementProvider.Key {
 
     BOOLEAN("boolean"),
     INTEGER("integer"),
     NUMBER("number"),
     STRING("string");
 
-    override fun toString(): kotlin.String = key
+    override fun toString(): String = key
 
-    companion object : EnumElementProvider<DataType>(info = info())
+    companion object : EnumElementProvider<DataType>(info = info()) {
+        @JvmStatic
+        @JsonCreator
+        fun creator(name: String) = DataType.orThrow(name)
+    }
 }
