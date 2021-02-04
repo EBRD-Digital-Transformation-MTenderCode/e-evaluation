8eec979f438623239e83b76b86a9854416c4116d
Evgen Kopach <zkopach@gmail.com>
2021-02-04 17:29:37 +0200

    Removed redundant container interactor object creation

--- src/test/kotlin/com/procurement/evaluation/infrastructure/repository/CassandraContainer.kt
+++ src/test/kotlin/com/procurement/evaluation/infrastructure/repository/CassandraContainer.kt
@@ -6,10 +6,11 @@ import org.testcontainers.containers.wait.strategy.Wait
 object CassandraContainer {
 
     private val initialScripts = readFile("docs/data.cql")
+    private val containerInteractor: CassandraContainerInteractor by lazy { CassandraContainerInteractor(container) }
 
     val container: CassandraTestContainer = CassandraTestContainer("3.11").apply { run() }
         get() {
-            CassandraContainerInteractor(field).cqlsh(initialScripts)
+            containerInteractor.cqlsh(initialScripts)
             return field
         }
 
