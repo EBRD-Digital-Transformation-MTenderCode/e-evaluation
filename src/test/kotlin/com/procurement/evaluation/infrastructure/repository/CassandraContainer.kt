e65fb847a1bbd041f196290e9c0be2acd988c820
Evgen Kopach <zkopach@gmail.com>
2021-02-12 12:19:50 +0200

    Fixed problem with lazy initialization

--- src/test/kotlin/com/procurement/evaluation/infrastructure/repository/CassandraContainer.kt
+++ src/test/kotlin/com/procurement/evaluation/infrastructure/repository/CassandraContainer.kt
@@ -6,11 +6,10 @@ import org.testcontainers.containers.wait.strategy.Wait
 object CassandraContainer {
 
     private val initialScripts = readFile("docs/data.cql")
-    private val containerInteractor: CassandraContainerInteractor by lazy { CassandraContainerInteractor(container) }
 
     val container: CassandraTestContainer = CassandraTestContainer("3.11").apply { run() }
         get() {
-            containerInteractor.cqlsh(initialScripts)
+            CassandraContainerInteractor(field).cqlsh(initialScripts)
             return field
         }
 
