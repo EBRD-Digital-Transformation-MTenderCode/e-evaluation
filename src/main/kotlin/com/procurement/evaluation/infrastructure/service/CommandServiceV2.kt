2ee3840b92fef172b19f72d10bd929ab453135e4
katherine <kate.shaidurova@gmail.com>
2021-03-04 17:09:41 +0200

    ES-5670 Added call to handler handle method

--- src/main/kotlin/com/procurement/evaluation/infrastructure/service/CommandServiceV2.kt
+++ src/main/kotlin/com/procurement/evaluation/infrastructure/service/CommandServiceV2.kt
@@ -12,6 +12,7 @@ import com.procurement.evaluation.infrastructure.handler.v2.CheckRelatedTenderer
 import com.procurement.evaluation.infrastructure.handler.v2.CloseAwardPeriodHandler
 import com.procurement.evaluation.infrastructure.handler.v2.CreateAwardHandler
 import com.procurement.evaluation.infrastructure.handler.v2.CreateUnsuccessfulAwardsHandler
+import com.procurement.evaluation.infrastructure.handler.v2.DoConsiderationHandler
 import com.procurement.evaluation.infrastructure.handler.v2.FindAwardsForProtocolHandler
 import com.procurement.evaluation.infrastructure.handler.v2.GetAwardByIdsHandler
 import com.procurement.evaluation.infrastructure.handler.v2.GetAwardStateByIdsHandler
@@ -24,6 +25,7 @@ import org.springframework.stereotype.Service
 @Service
 class CommandServiceV2(
     private val logger: Logger,
+    private val doConsiderationHandler: DoConsiderationHandler,
     private val getAwardStateByIdsHandler: GetAwardStateByIdsHandler,
     private val getAwardByIdsHandler: GetAwardByIdsHandler,
     private val findAwardsForProtocolHandler: FindAwardsForProtocolHandler,
@@ -40,6 +42,7 @@ class CommandServiceV2(
 ) {
 
     fun execute(descriptor: CommandDescriptor): ApiResponseV2 = when (descriptor.action) {
+        CommandTypeV2.DO_CONSIDERATION -> doConsiderationHandler.handle(descriptor)
         CommandTypeV2.GET_AWARD_STATES_BY_IDS -> getAwardStateByIdsHandler.handle(descriptor)
         CommandTypeV2.GET_AWARD_BY_IDS -> getAwardByIdsHandler.handle(descriptor)
         CommandTypeV2.FIND_AWARDS_FOR_PROTOCOL -> findAwardsForProtocolHandler.handle(descriptor)
