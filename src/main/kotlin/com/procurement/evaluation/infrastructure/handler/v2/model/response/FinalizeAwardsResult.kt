20133ceb1e79cff8ab0772bcefb52be951bfcc65
katherine <kate.shaidurova@gmail.com>
2021-03-23 15:22:48 +0200

    ES-5809 Added result

--- src/main/kotlin/com/procurement/evaluation/infrastructure/handler/v2/model/response/FinalizeAwardsResult.kt
+++ src/main/kotlin/com/procurement/evaluation/infrastructure/handler/v2/model/response/FinalizeAwardsResult.kt
@@ -1,9 +1,8 @@
 package com.procurement.evaluation.infrastructure.handler.v2.model.response
 
 
+import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.annotation.JsonProperty
-import com.procurement.evaluation.domain.model.award.AwardId
-import com.procurement.evaluation.domain.model.bid.BidId
 import com.procurement.evaluation.model.dto.ocds.AwardStatus
 import com.procurement.evaluation.model.dto.ocds.AwardStatusDetails
 
@@ -11,9 +10,11 @@ data class FinalizeAwardsResult(
     @param:JsonProperty("awards") @field:JsonProperty("awards") val awards: List<Award>
 ) {
     data class Award(
-        @param:JsonProperty("id") @field:JsonProperty("id") val id: AwardId,
+        @param:JsonProperty("id") @field:JsonProperty("id") val id: String,
         @param:JsonProperty("status") @field:JsonProperty("status") val status: AwardStatus,
         @param:JsonProperty("statusDetails") @field:JsonProperty("statusDetails") val statusDetails: AwardStatusDetails,
-        @param:JsonProperty("relatedBid") @field:JsonProperty("relatedBid") val relatedBid: BidId
+
+        @JsonInclude(JsonInclude.Include.NON_NULL)
+        @param:JsonProperty("relatedBid") @field:JsonProperty("relatedBid") val relatedBid: String?
     )
 }
