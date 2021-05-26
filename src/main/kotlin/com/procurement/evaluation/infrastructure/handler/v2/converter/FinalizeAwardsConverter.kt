0ffc5dfadeafd0e5f1dd2b5d664e663508ee71be
Evgen Kopach <zkopach@gmail.com>
2021-05-26 17:53:54 +0300

    ES-6174 Updated 'finalizeAwards' command. Changed request model. Updated rules

--- src/main/kotlin/com/procurement/evaluation/infrastructure/handler/v2/converter/FinalizeAwardsConverter.kt
+++ src/main/kotlin/com/procurement/evaluation/infrastructure/handler/v2/converter/FinalizeAwardsConverter.kt
@@ -2,34 +2,26 @@ package com.procurement.evaluation.infrastructure.handler.v2.converter
 
 import com.procurement.evaluation.application.model.award.finalize.FinalizeAwardsParams
 import com.procurement.evaluation.application.model.parseCpid
-import com.procurement.evaluation.application.model.parseLotId
 import com.procurement.evaluation.application.model.parseOcid
+import com.procurement.evaluation.domain.model.award.AwardId
 import com.procurement.evaluation.infrastructure.fail.error.DataErrors
 import com.procurement.evaluation.infrastructure.handler.v2.model.request.FinalizeAwardsRequest
 import com.procurement.evaluation.lib.functional.Result
 import com.procurement.evaluation.lib.functional.asSuccess
 
 fun FinalizeAwardsRequest.convert(): Result<FinalizeAwardsParams, DataErrors> {
-    val parsedCpid = parseCpid(cpid)
-        .onFailure { return it }
-    val parsedOcid = parseOcid(ocid)
-        .onFailure { return it }
+    val parsedCpid = parseCpid(cpid).onFailure { return it }
+    val parsedOcid = parseOcid(ocid).onFailure { return it }
 
     return FinalizeAwardsParams(
         cpid = parsedCpid,
         ocid = parsedOcid,
-        tender = tender.convert()
-            .onFailure { return it }
+        contracts = contracts.map { it.convert().onFailure { return it } }
     ).asSuccess()
 }
 
-private fun FinalizeAwardsRequest.Tender.convert(): Result<FinalizeAwardsParams.Tender, DataErrors> =
-    FinalizeAwardsParams.Tender(
-        lots = lots.map { it.convert().onFailure { return it } }
-    ).asSuccess()
+private fun FinalizeAwardsRequest.Contract.convert(): Result<FinalizeAwardsParams.Contract, DataErrors> {
+    val parsedAwardId = AwardId.fromString(awardId)
 
-private fun FinalizeAwardsRequest.Tender.Lot.convert(): Result<FinalizeAwardsParams.Tender.Lot, DataErrors> =
-    FinalizeAwardsParams.Tender.Lot(
-        id = parseLotId(id, "tender.lots.id").onFailure { return it }
-    ).asSuccess()
-
+    return FinalizeAwardsParams.Contract(id = id, awardId = parsedAwardId).asSuccess()
+}
